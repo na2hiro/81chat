c2e89f2e7a7ef50e02ef128a99b896bfabb4d468
na2hiro <na2hiro@192.168.0.2>
2011-04-18 19:51:06 +0900

    はじめてのコミット

--- chat.php
+++ chat.php
@@ -1,11 +1,15 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+if($_SERVER["REMOTE_ADDR"]=="61.27.73.10") printError("damn!");
+
 define("DELETE_SPAN",60);
-define("LOG_MAX",30);
+define("LOG_MAX",1000);
+define("LOG_DEFAULT",30);
 define("DB_USER_TABLE", "testchatuser");
 define("DB_LOG_TABLE", "testchat");
 $now=time();
 
-$conn = mysql_connect('localhost', 'user', 'password') or die(mysql_error());
+$conn = mysql_connect('localhost', 'user', 'pass') or die(mysql_error());
 mysql_select_db('chat') or die(mysql_error());
 mysql_query("SET NAMES utf8");
 if(isset($_REQUEST['sessionid']) && $_REQUEST['sessionid']!=""){
@@ -36,25 +40,30 @@ if(isset($_REQUEST['login'])){
 	$myid=(int)mysql_insert_id();
 	$retarray['id']=$myid;
 	$_SESSION['userid']=$myid;
-	
+	comment("<span class='in'>■入室通知</span>", "「".$name."」さんが入室", $ip);
+
 }else if(isset($_REQUEST['logout']) && $myid!=NULL){
+	$sql="SELECT ip, name FROM ".DB_USER_TABLE." WHERE id = {$myid}";
+	$res=mysql_query($sql);
+	$row = mysql_fetch_assoc($res);
+	$ip=$row['ip'];
+	$name=$row['name'];
+
 	$sql="DELETE FROM ".DB_USER_TABLE." WHERE id = {$myid}";
 	mysql_query($sql);
 	unset($_SESSION['userid']);
 	$myid=NULL;
+	comment("<span class='out'>■退室通知</span>", "「".$name."」さんが退室", $ip);
 
 }else if(isset($_REQUEST['comment'])){
-	$comment=mysql_real_escape_string(htmlspecialchars($_REQUEST['comment']));
 	if($myid==NULL) printError("入室してください");
 	if(!is_numeric($myid)) printError("ユーザidが異常です");
 	$sql="SELECT name, ip FROM ".DB_USER_TABLE." WHERE id = {$myid}";
 	$res=mysql_query($sql);
 	$row = mysql_fetch_assoc($res);
 	if($row==false) printError("再入室してください");
-	$name=mysql_real_escape_string($row['name']);
-	$ip=$row['ip'];
-	$sql="INSERT INTO ".DB_LOG_TABLE." VALUES(NULL, {$now}, '{$name}', '{$comment}', {$ip})";
-	mysql_query($sql) or printError("えらーb ".mysql_error().$sql);	
+
+	comment($row['name'], htmlspecialchars($_REQUEST['comment']), $row['ip']);
 }
 
 if($myid!=NULL){
@@ -72,7 +81,14 @@ try{
 }
 
 $deletebefore=$now-DELETE_SPAN;
+$sql="SELECT name, ip FROM ".DB_USER_TABLE." WHERE last < {$deletebefore}";
+$res=mysql_query($sql);
+while ($row = mysql_fetch_assoc($res)) {
+	comment("<span class='out'>■失踪通知</span>", "「".$row['name']."」さんいない", $row['ip']);
+}
+
 $sql="DELETE FROM ".DB_USER_TABLE." WHERE last < {$deletebefore}";
+
 mysql_query($sql);
 $sql="SELECT * FROM ".DB_USER_TABLE." ORDER BY id";
 $res=mysql_query($sql);
@@ -97,7 +113,8 @@ if(!is_numeric($_REQUEST['last'])){
 	$res=mysql_query($sql);
 	if($res==false) printError(mysql_error());
 	$row=mysql_fetch_assoc($res);
-	$id=max(0, $row['hoge']-LOG_MAX);
+	$logs=is_numeric($_REQUEST['recent'])?max(0, min($_REQUEST['recent'], LOG_MAX)):LOG_DEFAULT;
+	$id=max(0, $row['hoge']-$logs);
 }
 
 $retarray['myid']=$myid;
@@ -116,10 +133,27 @@ while ($row = mysql_fetch_assoc($res)) {
 }
 $retarray['sessionid']=session_id();
 $retarray['lastid']=(int)$id;
-echo json_encode($retarray);
-
+$echo=json_encode($retarray);
+if($_REQUEST['callback']){
+	echo $_REQUEST['callback']."(".$echo.")";
+}else{
+	echo $echo;
+}
 
 function printError($message){
-	echo json_encode(Array('error'=>true, 'errormessage'=>$message));
+	$echo=json_encode(Array('error'=>true, 'errormessage'=>$message));
+	if($_REQUEST['callback']){
+		echo $_REQUEST['callback']."(".$echo.")";
+	}else{
+		echo $echo;
+	}
 	exit;
+}
+
+function comment($name, $comment, $ip){
+	global $conn, $now;
+	$name=mysql_real_escape_string($name);
+	$comment=mysql_real_escape_string($comment);
+	$sql="INSERT INTO ".DB_LOG_TABLE." VALUES(NULL, {$now}, '{$name}', '{$comment}', {$ip})";
+	mysql_query($sql) or printError("えらーb ".mysql_error().$sql);
 }
